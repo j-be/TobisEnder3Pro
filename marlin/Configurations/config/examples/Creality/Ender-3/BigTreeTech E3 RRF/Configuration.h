1300b072c7401cc2b50284001f3783f4699f7f4d
Juri Berlanda <juriberlanda@hotmail.com>
2022-07-14 21:54:20 +0200

    [PID] Add values from first tune.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- marlin/Configurations/config/examples/Creality/Ender-3/BigTreeTech E3 RRF/Configuration.h
+++ marlin/Configurations/config/examples/Creality/Ender-3/BigTreeTech E3 RRF/Configuration.h
@@ -655,9 +655,9 @@
     #define DEFAULT_Ki_LIST {   1.54,   1.54 }
     #define DEFAULT_Kd_LIST {  76.55,  76.55 }
   #else
-    #define DEFAULT_Kp  21.73
-    #define DEFAULT_Ki   1.54
-    #define DEFAULT_Kd  76.55
+    #define DEFAULT_Kp  23.17
+    #define DEFAULT_Ki   1.92
+    #define DEFAULT_Kd  69.97
   #endif
 #endif
 
