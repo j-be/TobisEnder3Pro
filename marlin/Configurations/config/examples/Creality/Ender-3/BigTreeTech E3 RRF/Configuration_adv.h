12b21627547ff8d45854d8030e49ea17bb1f8324
Juri Berlanda <juriberlanda@hotmail.com>
2022-07-14 19:42:57 +0200

    [VoronM4] Set ESteps, invert direction, lower current, and SpreadCycle.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- marlin/Configurations/config/examples/Creality/Ender-3/BigTreeTech E3 RRF/Configuration_adv.h
+++ marlin/Configurations/config/examples/Creality/Ender-3/BigTreeTech E3 RRF/Configuration_adv.h
@@ -2896,7 +2896,7 @@
   #endif
 
   #if AXIS_IS_TMC(E0)
-    #define E0_CURRENT      650
+    #define E0_CURRENT      500
     #define E0_MICROSTEPS    16
     #define E0_RSENSE         0.11
     #define E0_CHAIN_POS     -1
@@ -3060,7 +3060,7 @@
   #define STEALTHCHOP_U
   #define STEALTHCHOP_V
   #define STEALTHCHOP_W
-  #define STEALTHCHOP_E
+  //#define STEALTHCHOP_E
 
   /**
    * Optimize spreadCycle chopper parameters by using predefined parameter sets
