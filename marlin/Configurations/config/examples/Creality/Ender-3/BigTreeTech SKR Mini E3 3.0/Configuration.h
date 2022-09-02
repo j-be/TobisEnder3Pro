6bda511a5544763de5070bd4aa4cbebb247b5ff0
Juri Berlanda <juriberlanda@hotmail.com>
2022-09-02 16:25:59 +0200

    [MiniE3v3] Set PID values for hotend and bed.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- marlin/Configurations/config/examples/Creality/Ender-3/BigTreeTech SKR Mini E3 3.0/Configuration.h
+++ marlin/Configurations/config/examples/Creality/Ender-3/BigTreeTech SKR Mini E3 3.0/Configuration.h
@@ -654,9 +654,9 @@
     #define DEFAULT_Ki_LIST {   1.08,   1.08 }
     #define DEFAULT_Kd_LIST { 114.00, 114.00 }
   #else
-    #define DEFAULT_Kp  21.73
-    #define DEFAULT_Ki   1.54
-    #define DEFAULT_Kd  76.55
+    #define DEFAULT_Kp  23.62
+    #define DEFAULT_Ki   1.82
+    #define DEFAULT_Kd  76.54
   #endif
 #endif
 
@@ -737,9 +737,9 @@
 
   // 120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   // from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 41.78
-  #define DEFAULT_bedKi 7.32
-  #define DEFAULT_bedKd 158.93
+  #define DEFAULT_bedKp 105.40
+  #define DEFAULT_bedKi 20.27
+  #define DEFAULT_bedKd 365.39
 
   // FIND YOUR OWN: "M303 E-1 C8 S90" to run autotune on the bed at 90 degreesC for 8 cycles.
 #endif // PIDTEMPBED
