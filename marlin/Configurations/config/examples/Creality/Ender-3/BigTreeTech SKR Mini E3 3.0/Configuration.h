4248f7184374823961e71d7b8a8ee4e78955ec58
Juri Berlanda <juriberlanda@hotmail.com>
2022-09-02 16:26:39 +0200

    [MiniE3v3] Set ESteps to 594.
    Signed-off-by: Juri Berlanda <juriberlanda@hotmail.com>

--- marlin/Configurations/config/examples/Creality/Ender-3/BigTreeTech SKR Mini E3 3.0/Configuration.h
+++ marlin/Configurations/config/examples/Creality/Ender-3/BigTreeTech SKR Mini E3 3.0/Configuration.h
@@ -1012,7 +1012,7 @@
  * Override with M92
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 565 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 594 }
 
 /**
  * Default Max Feed Rate (linear=mm/s, rotational=°/s)
